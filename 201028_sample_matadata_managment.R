c51356eb489093cc333cd4c5024d3d535ce362a2
Paul <paul.czechowski@abiori.org>
2020-10-28 16:28:23 +1300

    * **28-Oct-2020** - creating `/Users/paul/Documents/OU_eDNA/200901_scripts/201028_sample_matadata_managment.R`   * writing objects to `/Users/paul/Documents/OU_eDNA/201028_Robjects`   * Read cells from     * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200907_plate_layouts.xlsx`     * `/Users/paul/Documents/OU_eDNA/191031_primers/200302_Bunce_et_al_0000_MiFishEmod_single_step_primers.xlsx`   * output appropriate format for     * `/Users/paul/Documents/OU_eDNA/200128_lab_work/201028_Otago_Genomics_sample_info.xlsx`     * and later possibly the mapping file   * next: spread and subset for Otago Genomics - **pending**   * commit

---
+++ 201028_sample_matadata_managment.R
@@ -0,0 +1,136 @@
+# Fiordldand project 28-10-2020
+#
+# Read cells from 
+#   "/Users/paul/Documents/OU_eDNA/200128_lab_work/200907_plate_layouts.xlsx"
+#   "/Users/paul/Documents/OU_eDNA/191031_primers/200302_Bunce_et_al_0000_MiFishEmod_single_step_primers.xlsx"  
+# output appropriate format for
+#   "/Users/paul/Documents/OU_eDNA/200128_lab_work/201028_Otago_Genomics_sample_info.xlsx"
+#    and later possibly the mapping file
+
+
+## Environment
+## ===========
+
+rm(list = ls())
+
+library(tidyverse)
+
+
+## Read in sample data
+## ===================
+
+path <- c(
+  "/Users/paul/Documents/OU_eDNA/200128_lab_work/200907_plate_layouts.xlsx",
+  "/Users/paul/Documents/OU_eDNA/191031_primers/200302_Bunce_et_al_0000_MiFishEmod_single_step_primers.xlsx"
+  )
+
+# later read in barcodes as well?
+ 
+# pool plate U 1 samples and primers
+# ----------------------------------
+
+u1s <- readxl::read_excel(path[1], range = "A3:M11", .name_repair = "universal")
+u1pf <- readxl::read_excel(path[1], range = "A46:M54", .name_repair = "universal")
+u1pr <- readxl::read_excel(path[1], range = "A56:M64", .name_repair = "universal") 
+
+u1s <- u1s %>% pivot_longer(!pool.plate.U.1) %>% rename(row = pool.plate.U.1, col = name, content = value) %>% add_column(plate = "U.1", .before = TRUE) %>% add_column(type = "amplicon", .before = TRUE)
+u1pf <- u1pf %>% pivot_longer(!U.1.fwd) %>% rename(row = U.1.fwd, col = name, content = value) %>% add_column(plate = "U.1", .before = TRUE) %>% add_column(type = "f_primer", .before = TRUE)
+u1pr <- u1pr %>% pivot_longer(!U.1.rev) %>% rename(row = U.1.rev, col = name, content = value) %>% add_column(plate = "U.1", .before = TRUE) %>% add_column(type = "r_primer", .before = TRUE)
+
+# pool plate U 2 samples and primers
+# ---------------------------------
+
+u2s <- readxl::read_excel(path[1], range = "A13:M21", .name_repair = "universal") 
+u2pf <- readxl::read_excel(path[1], range = "A67:M75", .name_repair = "universal")
+u2pr <- readxl::read_excel(path[1], range = "A77:M85", .name_repair = "universal")
+
+u2s <- u2s %>% pivot_longer(!pool.plate.U.2) %>% rename(row = pool.plate.U.2, col = name, content = value) %>% add_column(plate = "U.2", .before = TRUE) %>% add_column(type = "amplicon", .before = TRUE)
+u2pf <- u2pf %>% pivot_longer(!U.2.fwd) %>% rename(row = U.2.fwd, col = name, content = value) %>% add_column(plate = "U.2", .before = TRUE) %>% add_column(type = "f_primer", .before = TRUE)
+u2pr <- u2pr %>% pivot_longer(!U.2.rev) %>% rename(row = U.2.rev, col = name, content = value) %>% add_column(plate = "U.2", .before = TRUE) %>% add_column(type = "r_primer", .before = TRUE)
+
+# pool plate E 1 samples and primers
+# ---------------------------------
+
+e1s <- readxl::read_excel(path[1], range = "A24:M32", .name_repair = "universal")
+e1pf <- readxl::read_excel(path[1], range = "A88:M96", .name_repair = "universal")
+e1pr <- readxl::read_excel(path[1], range = "A98:M106", .name_repair = "universal")
+
+e1s <- e1s %>% pivot_longer(!pool.plate.E.1) %>% rename(row = pool.plate.E.1, col = name, content = value) %>% add_column(plate = "E.1", .before = TRUE) %>% add_column(type = "amplicon", .before = TRUE)
+e1pf <- e1pf %>% pivot_longer(!E.1.fwd) %>% rename(row = E.1.fwd, col = name, content = value) %>% add_column(plate = "E.1", .before = TRUE) %>% add_column(type = "f_primer", .before = TRUE)
+e1pr <- e1pr %>% pivot_longer(!E.1.rev) %>% rename(row = E.1.rev, col = name, content = value) %>% add_column(plate = "E.1", .before = TRUE) %>% add_column(type = "r_primer", .before = TRUE)
+
+# pool plate E 2 samples and primers
+# ----------------------------------
+
+e2s <- readxl::read_excel(path[1], range = "A34:M42", .name_repair = "universal")
+e2pf <- readxl::read_excel(path[1], range = "A109:M117", .name_repair = "universal")
+e2pr <- readxl::read_excel(path[1], range = "A119:M127", .name_repair = "universal")
+
+e2s <- e2s %>% pivot_longer(!pool.plate.E.2 ) %>% rename(row = pool.plate.E.2, col = name, content = value) %>% add_column(plate = "E.2", .before = TRUE) %>% add_column(type = "amplicon", .before = TRUE) 
+e2pf <- e2pf %>% pivot_longer(!E.2.fwd) %>% rename(row = E.2.fwd, col = name, content = value) %>% add_column(plate = "E.2", .before = TRUE) %>% add_column(type = "f_primer", .before = TRUE)
+e2pr <- e2pr %>% pivot_longer(!E.2.rev) %>% rename(row = E.2.rev, col = name, content = value) %>% add_column(plate = "E.2", .before = TRUE) %>% add_column(type = "r_primer", .before = TRUE)
+
+# get large data frames for further manipulation
+# ---------------------------------------------
+
+amplicon_positions <- bind_rows(u1s, u2s, e1s, e2s) %>% 
+  mutate(col = str_remove(col, "...")) %>% 
+  mutate(col, col = as.integer(col)) %>% 
+  mutate(key = paste0(plate,".",row,".",col)) %>% print(n = Inf)
+save(amplicon_positions, file = "/Users/paul/Documents/OU_eDNA/201028_Robjects/201028_sample_managment__amplicon_positions")
+
+primer_positions <- bind_rows(u1pf, u1pr, u2pf, u2pr, e1pf, e1pr, e2pf, e2pr) %>% 
+  mutate(col = str_remove(col, "...")) %>% 
+  mutate(col, col = as.integer(col)) %>% 
+  mutate(key = paste0(plate,".",row,".",col))
+save(primer_positions, file = "/Users/paul/Documents/OU_eDNA/201028_Robjects/201028_sample_managment__primer_positions")
+
+## Read in primer data
+## ===================
+
+up5 <- readxl::read_excel(path[2], range = "E4:J11", .name_repair = "universal",  col_names = c("Name", "Adapter",	"FluPad", "Index", "TemplatePrimer", "CompletePrimer"))
+up7 <- readxl::read_excel(path[2], range = "E16:J32", .name_repair = "universal", col_names = c("Name", "Adapter",	"FluPad", "Index", "TemplatePrimer", "CompletePrimer"))
+
+ep5 <- readxl::read_excel(path[2], range = "E40:J47", .name_repair = "universal", col_names = c("Name", "Adapter",	"FluPad", "Index", "TemplatePrimer", "CompletePrimer"))
+ep7 <- readxl::read_excel(path[2], range = "E52:J68", .name_repair = "universal", col_names = c("Name", "Adapter",	"FluPad", "Index", "TemplatePrimer", "CompletePrimer"))
+
+# get large data frame for further manipulation
+# ---------------------------------------------
+
+primer_sequences <- bind_rows(up5, up7, ep5, ep7)
+save(primer_sequences, file = "/Users/paul/Documents/OU_eDNA/201028_Robjects/201028_sample_managment__primers")
+
+
+## Read amplicon concentration data
+## ================================
+
+# **pending**
+
+
+## Merge data for further fomatting
+## =======================================================================
+
+big_table <- left_join(amplicon_positions,  primer_positions, by=c("key"), copy = TRUE, Keep = TRUE) %>% print(n = Inf)
+big_table <- left_join(big_table,  primer_sequences, by=c("content.y" = "Name"), copy = TRUE, Keep = TRUE) %>% print(n = Inf)
+
+
+## Write Excel Sheet compatible with Otago genomics sample submssion sheet
+## =======================================================================
+
+# **pending**
+
+
+pivot_wider(og_big_table, names_from = c(type))
+
+## Write mapping file for Qiime imports or other sequenc processing
+## ================================================================
+
+# **pending**
+
+
+
+primer_plates %>% filter(type %in% c("f_primer","fr_primer"))
+
+primer_plates
+
+pool_plates$type
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Fiorldand project utility scripts
+
 * **1-Sep-2020**
   * creating `/Users/paul/Documents/OU_eDNA/200901_scripts/200901_quantification_calibration.R`
     * associating fluorescence signal with DNA concentrations, lab book page 48
@@ -13,4 +15,21 @@
     * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200916_Eplate1_ng-ul.xlsx`
     * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200916_Eplate2_ng-ul.xlsx
     * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200916_Uplate1_ng-ul.xlsx
-    * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200916_Uplate2_ng-ul.xlsx` 
+    * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200916_Uplate2_ng-ul.xlsx`
+  * commit `e277b04a54b5599ad3ce94ad0a7be375335e211d`
+* **28-Oct-2020** - creating `/Users/paul/Documents/OU_eDNA/200901_scripts/201028_sample_matadata_managment.R`
+  * writing objects to `/Users/paul/Documents/OU_eDNA/201028_Robjects`
+  * Read cells from
+    * `/Users/paul/Documents/OU_eDNA/200128_lab_work/200907_plate_layouts.xlsx`
+    * `/Users/paul/Documents/OU_eDNA/191031_primers/200302_Bunce_et_al_0000_MiFishEmod_single_step_primers.xlsx`
+  * output appropriate format for
+    * `/Users/paul/Documents/OU_eDNA/200128_lab_work/201028_Otago_Genomics_sample_info.xlsx`
+    * and later possibly the mapping file
+  * next: spread and subset for Otago Genomics - **pending**
+
+  
+* **unfinished**
+  * `/Users/paul/Documents/OU_eDNA/200901_scripts/201028_sample_matadata_managment.R`
+    * use for creation of mapping file - **pending**
+    * read in library concentrations befor pooling for mapping file  - **pending**
+
